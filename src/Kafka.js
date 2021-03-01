06d9559cc22f49ba9dbd79766d31b0f064f73e29
hivemind-js <jan.schulte@hivemindtechnologies.com>
2021-03-01 13:26:43 +0000

    Add transactions

--- src/Kafka.js
+++ src/Kafka.js
@@ -8,4 +8,12 @@ exports.connectImpl = (producer) => () => producer.connect()
 
 exports.sendImpl = (producer, data) => () => producer.send(data)
 
+exports.sendTImpl = (transaction, data) => () => transaction.send(data)
+
 exports.disconnectImpl = (producer) => () => producer.disconnect()
+
+exports.transactionImpl = (producer) => () => producer.transaction()
+
+exports.commitImpl = (transaction) => () => transaction.commit()
+
+exports.abortImpl = (transaction) => () => transaction.abort()
